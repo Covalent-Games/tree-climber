87fb626ea44c4a0879898aec414505fc61d0475f
Jesse Pitel <jesse.pitel@gmail.com>
2015-07-17 20:13:57 -1000

    First commit

---
+++ Assets/Scripts/BranchScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BranchScript : MonoBehaviour {
+
+	public bool IsInstantiated = false; 
+}
---
+++ Assets/Scripts/TreeManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TreeManager : MonoBehaviour {
+
+	public List<GameObject> UnusedBranches;
+	public int BranchCount = 8;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	void CreateBranch(bool rightSide) {
+
+		GameObject newBranch = null;
+		while (newBranch == null) {
+			foreach (var branch in UnusedBranches) {
+				if (!branch.GetComponent<BranchScript>().IsInstantiated) {
+					newBranch = branch;
+					break;
+				}
+			}
+		}
+
+		if (rightSide) {
+			newBranch.transform.Translate(5, newBranch.transform.position.y + 12, 0);
+		}
+		else {
+			newBranch.transform.Translate(-5, newBranch.transform.position.y + 12, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
