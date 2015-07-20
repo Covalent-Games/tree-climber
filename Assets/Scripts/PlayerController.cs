ff3cf0a3523240c4869cc161983a161adf67bc3d
Jesse Pitel <jesse.pitel@gmail.com>
2015-07-19 18:42:22 -1000

    Started testing 3D tree layout.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Position;
+	bool touching = false;
+	Vector2 startTouchPosition = new Vector2();
+	bool swiped = false;
+
+	// Use this for initialization
+	void Start () {
+		Position = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Application.isEditor) {
+			if (Input.GetMouseButton(0)) {
+
+				if (!swiped) {
+					if (!touching) {
+						touching = true;
+						startTouchPosition = new Vector2(Input.mousePosition.x, 0);
+					} else if (Input.mousePosition.x - startTouchPosition.x > 10) {
+						Position += 90f;
+						Position = Position % 360f;
+						swiped = true;
+					} else if (Input.mousePosition.x - startTouchPosition.x < -10) {
+						Position -= 90f;
+						if (Position < 0)
+							Position = 360 - (Mathf.Abs(Position) % 360);
+						swiped = true;
+					}
+				}
+
+			} else if (touching) {
+				touching = false;
+				swiped = false;
+			}
+		} else {
+
+
+			if (Input.touchCount > 0) {
+				if (!swiped) {
+					var touch = Input.touches[0];
+
+					if (!touching) {
+						touching = true;
+						startTouchPosition = touch.position;
+					} else if (touch.position.x - startTouchPosition.x > 10) {
+						Position += 90f;
+						Position = Position % 360f;
+						swiped = true;
+					} else if (touch.position.x - startTouchPosition.x < -10) {
+						Position -= 90f;
+						if (Position < 0)
+							Position = 360 - (Mathf.Abs(Position) % 360);
+						swiped = true;
+					}
+				}
+
+			} else if (touching) {
+				touching = false;
+				swiped = false;
+			}
+		}
+
+		//if (Input.GetKeyDown("left")) {
+		//	Position += 90f;
+		//	Position = Position % 360f;
+		//} else if (Input.GetKeyDown("right")) {
+		//	Position -= 90f;
+		//	if (Position < 0)
+		//		Position = 360 - (Mathf.Abs(Position) % 360);
+		//	Debug.Log("Spin from " + transform.parent.rotation.eulerAngles.y + " to " + Position);
+		//}
+		Spin();
+		
+		////Rotation controls B
+		//if (Input.GetKey("left")) {
+		//	transform.parent.Rotate(Vector3.up, 70 * Time.deltaTime);
+		//} else if (Input.GetKey("right")) {
+		//	transform.parent.Rotate(Vector3.up, -70 * Time.deltaTime);
+		//}
+		//if (Input.GetKey("up")) {
+		//	transform.parent.Translate(0, 10 * Time.deltaTime, 0);
+		//} else if (Input.GetKey("down")) {
+		//	transform.parent.Translate(0, -10 * Time.deltaTime, 0);
+		//}
+		
+	}
+
+	void Spin() {
+		if (transform.parent.rotation.eulerAngles != new Vector3(0, Position, 0)) {
+			//Debug.Break();
+			float spinTo = Position;
+			if (Position == 0 && transform.parent.rotation.eulerAngles.y > 180) {
+				spinTo = 360;
+			} else if (Position == 270 && transform.parent.rotation.eulerAngles.y < 90) {
+				Debug.Log("BLARGH!");
+				transform.parent.rotation = Quaternion.Euler(new Vector3(0, 360 - 300 * Time.deltaTime, 0));
+				return;
+			}
+			transform.parent.rotation = Quaternion.Euler(Vector3.MoveTowards(transform.parent.rotation.eulerAngles, new Vector3(0, spinTo, 0), 500 * Time.deltaTime));
+		}
+	}
+}
--- Assets/Scripts/TreeManager.cs
+++ Assets/Scripts/TreeManager.cs
@@ -5,36 +5,59 @@ using System.Collections.Generic;
 public class TreeManager : MonoBehaviour {
 
 	public List<GameObject> UnusedBranches;
+	public List<GameObject> UsedBranches;
 	public int BranchCount = 8;
+
+	float BranchTimer = 0;
+	public float BranchInterval;
+
 
 	// Use this for initialization
 	void Start () {
 
-
+	
 	}
 
-	void CreateBranch(bool rightSide) {
 
-		GameObject newBranch = null;
-		while (newBranch == null) {
-			foreach (var branch in UnusedBranches) {
-				if (!branch.GetComponent<BranchScript>().IsInstantiated) {
-					newBranch = branch;
-					break;
-				}
-			}
-		}
-
-		if (rightSide) {
-			newBranch.transform.Translate(5, newBranch.transform.position.y + 12, 0);
-		}
-		else {
-			newBranch.transform.Translate(-5, newBranch.transform.position.y + 12, 0);
-		}
+	void IncrementBranchTimer() {
+		BranchTimer += Time.deltaTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		IncrementBranchTimer();
+
+		if (BranchTimer > BranchInterval) {
+			BranchTimer = 0;
+			if (UnusedBranches.Count > 1) {
+				GameObject branch1 = UnusedBranches[0];
+				GameObject branch2 = UnusedBranches[1];
+				UsedBranches.Add(branch1);
+				UsedBranches.Add(branch2);
+				UnusedBranches.Remove(branch1);
+				UnusedBranches.Remove(branch2);
+
+				int rand = Random.Range(0, 3);
+				branch1.transform.Rotate(0, rand * 90, 0);
+				int rand2 = Random.Range(0, 3);
+				if (rand2 == rand) {
+					rand2 = (rand2 + Random.Range(1,3)) % 4;
+				}
+				branch2.transform.Rotate(0, rand2 * 90, 0);
+			}
+		}
+		foreach (var branch in UsedBranches) {
+			branch.transform.position = new Vector3(0, branch.transform.position.y - (Time.deltaTime * 2), 0);
+			//branch.transform.Translate(branch.transform.position.x, branch.transform.position.y - (Time.deltaTime), branch.transform.position.z);
+			if (branch.transform.position.y < -0.6) {
+				branch.transform.position = new Vector3(0, 0.75f, 0);
+				UnusedBranches.Add(branch);
+			}
+		}
+		foreach (var branch in UnusedBranches) {
+			if (UsedBranches.Contains(branch))
+				UsedBranches.Remove(branch);
+		}
 	}
 }
